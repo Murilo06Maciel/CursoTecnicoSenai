a3907f2f3429d94e4c272184706bc1fdc438cd47
Murilo Maciel <murilomaciel06oficial@gmail.com>
2025-03-27 14:01:07 -0300

    iniciando DATABASE Biblioteca

---
+++ Banco de dados/DATABASE Biblioteca.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE Biblioteca;
+USE Biblioteca;
+
+CREATE TABLE autores(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    nacionalidade VARCHAR(100)
+);
+
+CREATE TABLE categorias (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE livros (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    id_autor INT,
+    id_categoria INT,
+    id_publicacao INT,
+    FOREIGN KEY (id_autor) REFERENCES autores(id),
+    FOREIGN KEY (id_categoria) REFERENCES categorias(id)
+);
+
+CREATE TABLE usuarios (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20),
+    endereco TEXT
+);
+
+CREATE TABLE emprestimos (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
+    id_livro INT,
+    data_retirada TEXT,
+    data_devolucao TEXT,
+    status ENUM ('Em andamento', 'Concluído'),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id),
+    FOREIGN KEY (id_livro) REFERENCES livros(id)
+);
+
+INSERT INTO autores (nome, nacionalidade) VALUES
+('Machado de Assis', 'Brasileiro'),
+('George Orwell', 'Britânico'),
+('J. K. Rowling', 'Britânica');
+
+INSERT INTO categorias (nome) VALUES
+('Romance'),
+('Ficção Científica'),
+('Fantasia');
