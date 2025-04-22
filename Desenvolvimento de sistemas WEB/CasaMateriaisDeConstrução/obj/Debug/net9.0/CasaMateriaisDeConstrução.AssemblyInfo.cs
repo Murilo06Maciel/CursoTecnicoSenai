5d9bd072d9d51c400a3d9f01e806c45b523f9087
Murilo Maciel <murilomaciel06oficial@gmail.com>
2025-04-22 13:59:57 -0300

    adicionando imagens

--- Desenvolvimento de sistemas WEB/CasaMateriaisDeConstrução/obj/Debug/net9.0/CasaMateriaisDeConstrução.AssemblyInfo.cs
+++ Desenvolvimento de sistemas WEB/CasaMateriaisDeConstrução/obj/Debug/net9.0/CasaMateriaisDeConstrução.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasaMateriaisDeConstrução")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164329f15fb7ac4fb10e17c568e58e6f434ea150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e5308a47aae04034598487fd4d9c07ee4e4d6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasaMateriaisDeConstrução")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasaMateriaisDeConstrução")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
